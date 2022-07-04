fd4825826e131f9981b87dd6abd91375b6354981
Dmitry Filatov <nabos.dezigner@gmail.com>
2022-07-04 12:39:33 +0300

    feat: add provider doc

--- lib/types.dart
+++ lib/types.dart
@@ -1,11 +1,22 @@
-import 'package:flutter_simple_form/subjects/subjects.dart';
+import 'package:flutter/widgets.dart' show BuildContext;
+import 'package:flutter_simple_form/flutter_simple_form.dart';
 
+/// Registered fields in the [SFormController].
 typedef SFormFieldsSubjects = Map<String, SFormFieldSubject>;
+
+/// State of fields [SFormFieldSubject].
 typedef SFormFields = Map<String, SFormFieldState>;
 
+/// Current errors in the fields [SFormFieldState.errorMessage].
 typedef SFormErrorValues = Map<String, String>;
+
+/// Current values in the fields [SFormFieldState.value].
+typedef SFormValues = Map<String, dynamic>;
+
+/// Function for validating fields, see [SFormValidationResolver.validate].
 typedef SFormValidationHandler = Future<SFormErrorValues> Function(
   SFormValues values,
 );
 
-typedef SFormValues = Map<String, dynamic>;
+/// Function for create [SFormController].
+typedef SFormControllerCreate = SFormController Function(BuildContext context);
